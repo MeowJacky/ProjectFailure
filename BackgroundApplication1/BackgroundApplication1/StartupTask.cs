e9004c59bdc2fc6ee337522944f2e6083d99552e
MeowJacky <59010641+MeowJacky@users.noreply.github.com>
2023-11-15 17:35:50 +0800

    rfid

--- BackgroundApplication1/BackgroundApplication1/StartupTask.cs
+++ BackgroundApplication1/BackgroundApplication1/StartupTask.cs
@@ -7,10 +7,31 @@ using Windows.ApplicationModel.Background;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
+using System.Diagnostics;
+using System.Threading.Tasks;
+using GrovePi;
+using GrovePi.Sensors;
+
 namespace BackgroundApplication1
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        private static SerialComms uartComms;
+        private static string rfid = "";
+        private void sleep(int s)
+        {             
+            Task.Delay(s).Wait();
+        }
+        static void UartDataHandler(Object sender, SerialComms.UartEventArgs e)
+        {
+            rfid = e.data;
+            Debug.WriteLine("Card Detected: " + rfid);
+        }
+        private void StartUart()
+        {
+            uartComms = new SerialComms();
+            uartComms.UartEvent += new SerialComms.UartEventDelegate(UartDataHandler);
+        }
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             // 
@@ -20,6 +41,13 @@ namespace BackgroundApplication1
             // from closing prematurely by using BackgroundTaskDeferral as
             // described in http://aka.ms/backgroundtaskdeferral
             //
+            StartUart();
+            while (true)
+            {
+                sleep(200);
+
+                rfid = "";
+            }
         }
     }
 }
