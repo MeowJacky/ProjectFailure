5ce15297da99840b1fc378a707c8725f4f13de8a
AutumnBoxes <yongsiu32123@gmail.com>
2023-12-06 17:55:12 +0800

    hi

--- Hardware/Forms1/Admin.Designer.cs
+++ Hardware/Forms1/Admin.Designer.cs
@@ -28,31 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.adminusername = new System.Windows.Forms.Label();
+            this.ClockedIn = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.ClockedIn)).BeginInit();
             this.SuspendLayout();
             // 
             // adminusername
             // 
             this.adminusername.AutoSize = true;
             this.adminusername.ForeColor = System.Drawing.Color.White;
-            this.adminusername.Location = new System.Drawing.Point(918, 35);
+            this.adminusername.Location = new System.Drawing.Point(816, 28);
             this.adminusername.Name = "adminusername";
-            this.adminusername.Size = new System.Drawing.Size(41, 20);
+            this.adminusername.Size = new System.Drawing.Size(38, 17);
             this.adminusername.TabIndex = 1;
             this.adminusername.Text = " bnjv";
             this.adminusername.Click += new System.EventHandler(this.label1_Click);
             // 
+            // ClockedIn
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.ClockedIn.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.ClockedIn.Legends.Add(legend1);
+            this.ClockedIn.Location = new System.Drawing.Point(251, 12);
+            this.ClockedIn.Name = "ClockedIn";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.ClockedIn.Series.Add(series1);
+            this.ClockedIn.Size = new System.Drawing.Size(300, 560);
+            this.ClockedIn.TabIndex = 2;
+            this.ClockedIn.Text = "chart1";
+            this.ClockedIn.Click += new System.EventHandler(this.chart1_Click_1);
+            // 
             // Admin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(1010, 610);
+            this.ClientSize = new System.Drawing.Size(1031, 643);
+            this.Controls.Add(this.ClockedIn);
             this.Controls.Add(this.adminusername);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Admin";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Admin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ClockedIn)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,5 +83,6 @@
 
         #endregion
         private System.Windows.Forms.Label adminusername;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ClockedIn;
     }
 }
--- Hardware/Forms1/Admin.cs
+++ Hardware/Forms1/Admin.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Forms1
 {
     public partial class Admin : Form
     {
+        private string strConnectionString = ConfigurationManager.ConnectionStrings["UserDB"].ConnectionString;
+
         private string username;
         public Admin(string username)
         {
@@ -19,7 +24,59 @@ namespace Forms1
             this.username = username;
 
             adminusername.Text = username;
+            PopulateChartData();
+
         }
+
+        private void PopulateChartData()
+        {
+            try
+            {
+                // Clear existing series from the chart
+                ClockedIn.Series.Clear();
+
+                using (SqlConnection connection = new SqlConnection(strConnectionString))
+                {
+                    connection.Open();
+
+                    string query = "SELECT Name, ClockIn FROM Admins";
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // Create a series for the chart
+                            Series series = new Series("Clock In Status");
+                            series.ChartType = SeriesChartType.Bar;
+
+                            while (reader.Read())
+                            {
+                                string adminName = reader["Name"].ToString();
+
+                                byte[] clockInBytes = (byte[])reader["ClockIn"];
+                                byte clockInValue = clockInBytes[0];
+
+                                // Add a data point to the series
+                                series.Points.AddXY(adminName, Convert.ToInt32(clockInValue));
+                            }
+
+
+
+
+
+                            // Add the series to the chart
+                            ClockedIn.Series.Add(series);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions appropriately
+                MessageBox.Show($"Error populating chart data: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
 
         private void chart1_Click(object sender, EventArgs e)
         {
@@ -37,6 +94,11 @@ namespace Forms1
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click_1(object sender, EventArgs e)
         {
 
         }
