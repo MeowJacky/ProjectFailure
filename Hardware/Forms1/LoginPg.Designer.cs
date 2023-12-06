58e85d7df3455caeb93051843c9a932b40184832
AutumnBoxes <yongsiu32123@gmail.com>
2023-12-06 16:25:02 +0800

    separate between admin and user

--- Hardware/Forms1/LoginPg.Designer.cs
+++ Hardware/Forms1/LoginPg.Designer.cs
@@ -116,6 +116,7 @@
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "LoginPg";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.LoginPg_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
