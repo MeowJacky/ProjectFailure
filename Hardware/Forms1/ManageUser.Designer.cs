8c0e15008af94df73c70c230f0b2af9279b0cbb8
Tristennnn <tristentay05@gmail.com>
2023-12-10 17:50:43 +0800

    Ok

--- Hardware/Forms1/ManageUser.Designer.cs
+++ Hardware/Forms1/ManageUser.Designer.cs
@@ -28,32 +28,195 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
+            this.userusername = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.userDBDataSet = new Forms1.UserDBDataSet();
+            this.adminsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.adminsTableAdapter = new Forms1.UserDBDataSetTableAdapters.AdminsTableAdapter();
+            this.uniqueUserIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nRICDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contactDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.departmentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.authorityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uniqueRFIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adminsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(44, 48);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.Size = new System.Drawing.Size(200, 60);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome, ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // userusername
+            // 
+            this.userusername.AutoSize = true;
+            this.userusername.Location = new System.Drawing.Point(146, 9);
+            this.userusername.Name = "userusername";
+            this.userusername.Size = new System.Drawing.Size(51, 20);
+            this.userusername.TabIndex = 1;
+            this.userusername.Text = "label2";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(16, 72);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(771, 401);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(763, 368);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Profile";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(763, 368);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "View clock in report";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.uniqueUserIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.nRICDataGridViewTextBoxColumn,
+            this.contactDataGridViewTextBoxColumn,
+            this.departmentDataGridViewTextBoxColumn,
+            this.authorityDataGridViewTextBoxColumn,
+            this.uniqueRFIDDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.adminsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(751, 356);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // userDBDataSet
+            // 
+            this.userDBDataSet.DataSetName = "UserDBDataSet";
+            this.userDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // adminsBindingSource
+            // 
+            this.adminsBindingSource.DataMember = "Admins";
+            this.adminsBindingSource.DataSource = this.userDBDataSet;
+            // 
+            // adminsTableAdapter
+            // 
+            this.adminsTableAdapter.ClearBeforeFill = true;
+            // 
+            // uniqueUserIDDataGridViewTextBoxColumn
+            // 
+            this.uniqueUserIDDataGridViewTextBoxColumn.DataPropertyName = "UniqueUserID";
+            this.uniqueUserIDDataGridViewTextBoxColumn.HeaderText = "UniqueUserID";
+            this.uniqueUserIDDataGridViewTextBoxColumn.Name = "uniqueUserIDDataGridViewTextBoxColumn";
+            this.uniqueUserIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // nRICDataGridViewTextBoxColumn
+            // 
+            this.nRICDataGridViewTextBoxColumn.DataPropertyName = "NRIC";
+            this.nRICDataGridViewTextBoxColumn.HeaderText = "NRIC";
+            this.nRICDataGridViewTextBoxColumn.Name = "nRICDataGridViewTextBoxColumn";
+            // 
+            // contactDataGridViewTextBoxColumn
+            // 
+            this.contactDataGridViewTextBoxColumn.DataPropertyName = "Contact";
+            this.contactDataGridViewTextBoxColumn.HeaderText = "Contact";
+            this.contactDataGridViewTextBoxColumn.Name = "contactDataGridViewTextBoxColumn";
+            // 
+            // departmentDataGridViewTextBoxColumn
+            // 
+            this.departmentDataGridViewTextBoxColumn.DataPropertyName = "Department";
+            this.departmentDataGridViewTextBoxColumn.HeaderText = "Department";
+            this.departmentDataGridViewTextBoxColumn.Name = "departmentDataGridViewTextBoxColumn";
+            // 
+            // authorityDataGridViewTextBoxColumn
+            // 
+            this.authorityDataGridViewTextBoxColumn.DataPropertyName = "Authority";
+            this.authorityDataGridViewTextBoxColumn.HeaderText = "Authority";
+            this.authorityDataGridViewTextBoxColumn.Name = "authorityDataGridViewTextBoxColumn";
+            // 
+            // uniqueRFIDDataGridViewTextBoxColumn
+            // 
+            this.uniqueRFIDDataGridViewTextBoxColumn.DataPropertyName = "UniqueRFID";
+            this.uniqueRFIDDataGridViewTextBoxColumn.HeaderText = "UniqueRFID";
+            this.uniqueRFIDDataGridViewTextBoxColumn.Name = "uniqueRFIDDataGridViewTextBoxColumn";
             // 
             // ManageUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(799, 494);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.userusername);
             this.Controls.Add(this.label1);
             this.Name = "ManageUser";
             this.Text = "ManageUser";
+            this.Load += new System.EventHandler(this.ManageUser_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adminsBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label userusername;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private UserDBDataSet userDBDataSet;
+        private System.Windows.Forms.BindingSource adminsBindingSource;
+        private UserDBDataSetTableAdapters.AdminsTableAdapter adminsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uniqueUserIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nRICDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contactDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn departmentDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn authorityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uniqueRFIDDataGridViewTextBoxColumn;
     }
 }
--- Hardware/Forms1/ManageUser.cs
+++ Hardware/Forms1/ManageUser.cs
@@ -20,11 +20,59 @@ namespace Forms1
         {
             InitializeComponent();
             this.username = username;
+
+            userusername.Text = username;
+
+            label1.Font = new Font(label1.Font.FontFamily, 14);
+            userusername.Font = new Font(userusername.Font.FontFamily, 14);
+
+            dataGridView1.AllowUserToAddRows = false;
+
+            DisplayUserInfo(username);
+
         }
 
+        private void DisplayUserInfo(string username)
+        {
+            string query = "SELECT * FROM Admins WHERE Name = @Username"; // Query to retrieve user info by username
+
+            using (SqlConnection connection = new SqlConnection(strConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+
+                    connection.Open();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable userData = new DataTable();
+                    adapter.Fill(userData);
+
+                    dataGridView1.DataSource = userData;
+                }
+            }
+        }
+    
         private void label1_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ManageUser_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'userDBDataSet.Admins' table. You can move, or remove it, as needed.
+            this.adminsTableAdapter.Fill(this.userDBDataSet.Admins);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
