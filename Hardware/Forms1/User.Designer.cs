0da5a1b3ebfbb9be9c3d42df9f986ba7a1f4650b
Tristennnn <tristentay05@gmail.com>
2024-01-17 13:03:07 +0800

    Db stuff

--- Hardware/Forms1/User.Designer.cs
+++ Hardware/Forms1/User.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.userusername = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.profileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -38,6 +39,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.ClockButton = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabControl1.SuspendLayout();
@@ -47,9 +50,9 @@
             // 
             this.userusername.AutoSize = true;
             this.userusername.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.userusername.Location = new System.Drawing.Point(561, 7);
+            this.userusername.Location = new System.Drawing.Point(631, 9);
             this.userusername.Name = "userusername";
-            this.userusername.Size = new System.Drawing.Size(46, 17);
+            this.userusername.Size = new System.Drawing.Size(51, 20);
             this.userusername.TabIndex = 0;
             this.userusername.Text = "label1";
             this.userusername.Click += new System.EventHandler(this.userusername_Click);
@@ -65,15 +68,14 @@
             this.projectWSYSToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(697, 36);
+            this.menuStrip1.Size = new System.Drawing.Size(784, 40);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // profileToolStripMenuItem
             // 
             this.profileToolStripMenuItem.Name = "profileToolStripMenuItem";
-            this.profileToolStripMenuItem.Size = new System.Drawing.Size(12, 32);
+            this.profileToolStripMenuItem.Size = new System.Drawing.Size(12, 36);
             this.profileToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.profileToolStripMenuItem.Click += new System.EventHandler(this.profileToolStripMenuItem_Click);
             // 
@@ -82,7 +84,7 @@
             this.projectWSYSToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.projectWSYSToolStripMenuItem.Margin = new System.Windows.Forms.Padding(100, 0, 0, 0);
             this.projectWSYSToolStripMenuItem.Name = "projectWSYSToolStripMenuItem";
-            this.projectWSYSToolStripMenuItem.Size = new System.Drawing.Size(150, 32);
+            this.projectWSYSToolStripMenuItem.Size = new System.Drawing.Size(178, 36);
             this.projectWSYSToolStripMenuItem.Text = "Project W-SYS";
             this.projectWSYSToolStripMenuItem.Click += new System.EventHandler(this.projectWSYSToolStripMenuItem_Click);
             // 
@@ -90,20 +92,20 @@
             // 
             this.logout.AutoSize = true;
             this.logout.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.logout.Location = new System.Drawing.Point(626, 7);
+            this.logout.Location = new System.Drawing.Point(704, 9);
             this.logout.Name = "logout";
-            this.logout.Size = new System.Drawing.Size(63, 17);
+            this.logout.Size = new System.Drawing.Size(71, 20);
             this.logout.TabIndex = 2;
             this.logout.Text = "Sign Out";
             this.logout.Click += new System.EventHandler(this.logout_Click);
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage2.Size = new System.Drawing.Size(689, 373);
+            this.tabPage2.Size = new System.Drawing.Size(776, 469);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Temperature";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -113,11 +115,11 @@
             // 
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.ClockButton);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tabPage1.Size = new System.Drawing.Size(689, 373);
+            this.tabPage1.Size = new System.Drawing.Size(776, 469);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Clock In";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -126,18 +128,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 23);
+            this.label1.Location = new System.Drawing.Point(9, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(182, 17);
+            this.label1.Size = new System.Drawing.Size(202, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "You have not clocked in yet";
             // 
             // ClockButton
             // 
-            this.ClockButton.Location = new System.Drawing.Point(7, 57);
+            this.ClockButton.Location = new System.Drawing.Point(8, 71);
             this.ClockButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ClockButton.Name = "ClockButton";
-            this.ClockButton.Size = new System.Drawing.Size(144, 38);
+            this.ClockButton.Size = new System.Drawing.Size(162, 48);
             this.ClockButton.TabIndex = 0;
             this.ClockButton.Text = "Clock in";
             this.ClockButton.UseVisualStyleBackColor = true;
@@ -146,24 +148,41 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(0, 34);
+            this.tabControl1.Location = new System.Drawing.Point(0, 42);
             this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(697, 402);
+            this.tabControl1.Size = new System.Drawing.Size(784, 502);
             this.tabControl1.TabIndex = 3;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(480, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "View all products";
             // 
             // User
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
-            this.ClientSize = new System.Drawing.Size(697, 436);
+            this.ClientSize = new System.Drawing.Size(784, 545);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.logout);
             this.Controls.Add(this.userusername);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "User";
             this.Text = "User";
             this.Load += new System.EventHandler(this.User_Load);
@@ -189,5 +208,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button ClockButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
     }
 }
