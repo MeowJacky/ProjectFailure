870c8b14f3d1d338075f90959f2ca28a0d965550
Tristennnn <tristentay05@gmail.com>
2024-01-20 15:52:08 +0800

    View products for user

--- Hardware/Forms1/Viewallproductsuser.Designer.cs
+++ Hardware/Forms1/Viewallproductsuser.Designer.cs
@@ -71,6 +71,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(18, 95);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(425, 350);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
