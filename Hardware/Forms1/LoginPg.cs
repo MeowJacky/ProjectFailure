c569c8a2fa60eda45b789894f7c198264c36b782
Tristennnn <tristentay05@gmail.com>
2024-01-21 20:36:52 +0800

    Scan rfid(dumb)

--- Hardware/Forms1/LoginPg.cs
+++ Hardware/Forms1/LoginPg.cs
@@ -20,7 +20,7 @@ namespace Forms1
 
         //HENG LE PC DONT LIKE DATACOMMS
         //DataComms dataComms = DataCommsHelper.GetDataCommsInstance();
-        //string rfidnum;
+        string rfidnum;
 
         //public delegate void myprocessDataDelegate(string strData);
         private DBTempUpdate temperatureUpdateService;
@@ -228,7 +228,156 @@ namespace Forms1
             forgetform.Show();
 
         }
+
+        //Below is some dumb code
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int currentStatus = GetCurrentClockStatus();
+
+            // Assuming that RFID data is received and processed in processDataReceive method
+            // You can modify this part based on your actual RFID data handling
+
+            // Check if the scanned RFID matches the RFID of the logged-in user
+
+            if (currentStatus == 0)
+            {
+                Console.WriteLine(rfidnum);
+                Console.WriteLine(IsRFIDMatch(rfidnum));
+                if (IsRFIDMatch(rfidnum))
+                {
+                    rfidnum = "";
+                    //dataComms.dataReceiveEvent -= commsdatareceive;
+                    //dataComms.dataSendErrorEvent -= commsSendError;
+                    DateTime currentTime = DateTime.Now;
+                    TimeSpan lateStartTime = new TimeSpan(9, 0, 0);
+                    TimeSpan lateEndTime = new TimeSpan(7, 59, 59);
+                    TimeSpan targetTime = new TimeSpan(8, 0, 0);
+
+                    if ((currentTime.TimeOfDay >= lateStartTime && currentTime.TimeOfDay <= TimeSpan.FromHours(24)) ||
+                        (currentTime.TimeOfDay >= TimeSpan.Zero && currentTime.TimeOfDay <= lateEndTime))
+                    {
+                        TimeSpan lateDuration = currentTime.TimeOfDay - targetTime;
+                        if (lateDuration < TimeSpan.Zero)
+                        {
+                            lateDuration = TimeSpan.FromHours(24) + lateDuration; // Adding a day for it to be positive
+                        }
+
+                        int lateHours = (int)lateDuration.TotalHours;
+                        int lateMinutes = lateDuration.Minutes;
+
+                        string lateMessage = "You are late by " + lateHours + " hours and " + lateMinutes + " minutes!";
+                        MessageBox.Show(lateMessage);
+                        UpdateClockStatus(1);
+                    }
+                }
+                //else
+                //{
+                //    MessageBox.Show("RFID does not match the logged-in user.");
+                //}
+            }
+            else
+            {
+                UpdateClockStatus(0);
+
+            }
+
+            RedirectBasedOnPriority();
+        }
+        private void RedirectBasedOnPriority()
+        {
+            // Query to retrieve the priority of the logged-in user
+            string query = "SELECT Authority FROM Admins WHERE UniqueRFID = @RFID";
+
+            using (SqlConnection connection = new SqlConnection(strConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@RFID", textBox1.Text);
+
+                    connection.Open();
+
+                    // Retrieve the priority value of the logged-in user from the database
+                    int userAuthority = Convert.ToInt32(command.ExecuteScalar());
+
+                    // Redirect to the respective page based on the priority
+                    if (userAuthority >= 1 && userAuthority <= 4)
+                    {
+                        // Admin
+                        Admin adminPage = new Admin(textBox1.Text, userAuthority);
+                        adminPage.Show();
+                        Console.WriteLine(userAuthority);
+                        SessionID sessionId = SessionID.Instance;
+                        sessionId.SetID(textBox1.Text, userAuthority);
+                    }
+                    else if (userAuthority == 999)
+                    {
+                        // User
+                        User userPage = new User(textBox1.Text);
+                        SessionID sessionId = SessionID.Instance;
+                        sessionId.SetID(tbUserName.Text);
+                        userPage.Show();
+                    }
+                    this.Hide();
+                }
+            }
+        }
+            private int GetCurrentClockStatus()
+        {
+            string query = "SELECT ClockIn FROM Admins WHERE UniqueRFID = @RFID"; // Query to retrieve ClockIn status by username
+
+            using (SqlConnection connection = new SqlConnection(strConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@RFID", textBox1.Text);
+
+                    connection.Open();
+
+                    return Convert.ToInt32(command.ExecuteScalar());
+                }
+            }
+        }
+
+        private void UpdateClockStatus(int status)
+        {
+            string updateQuery = "UPDATE Admins SET ClockIn = @Status WHERE UniqueRFID = @RFID"; // Update ClockIn status by username
+
+            using (SqlConnection connection = new SqlConnection(strConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(updateQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Status", status);
+                    command.Parameters.AddWithValue("@RFID", textBox1.Text);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private bool IsRFIDMatch(string scannedRFIDData)
+        {
+            // Query to retrieve the RFID of the logged-in user
+            string query = "SELECT UniqueRFID FROM Admins WHERE UniqueRFID = @RFID";
+
+            using (SqlConnection connection = new SqlConnection(strConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@RFID", textBox1.Text);
+
+                    connection.Open();
+
+                    // Retrieve the RFID value of the logged-in user from the database
+                    string userRFIDValue = Convert.ToString(command.ExecuteScalar());
+
+                    // Compare the scanned RFID value with the user's RFID value
+                    return scannedRFIDData == userRFIDValue;
+                }
+            }
+        }
     }
+
 
 }
 
